7236c4ca76039758794cea32de89f0ed4a569dd5
waylonggggg <tanwaylon38@gmail.com>
2026-05-11 22:29:30 +0800

    Implement bus-timings api endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,27 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import 'dotenv/config'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+
+app.get('bus-timings', async (c) => {
+  const url: string = "https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival";
+  const busStopCode: number = 83139;
+
+  const response = await fetch(url + `?BusStopCode=${busStopCode}`,
+    { headers: {"AccountKey": process.env.LTA_DATAMALL_API_KEY!}}
+  );
+
+  if (!response.ok) {
+    console.log("Error in fetching bus timings");
+  }
+
+  return c.json(response.json());
 })
 
 serve({
