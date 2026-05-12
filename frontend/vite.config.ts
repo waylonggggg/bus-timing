4a3447490dfece150dd6076f3523ff3dbdc75d71
waylonggggg <tanwaylon38@gmail.com>
2026-05-13 03:53:38 +0800

    Add cors middleware

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    cors: false, // to not conflict with hono cors middleware
+  },
   plugins: [
     react(),
     tailwindcss()],
