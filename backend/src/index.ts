88630165e6958f652d78fec27ff98a9daa505ba3
waylonggggg <tanwaylon38@gmail.com>
2026-05-12 23:56:42 +0800

    Add error handling in bus timings endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import 'dotenv/config'
+import { HTTPException } from 'hono/http-exception'
+import type { ContentfulStatusCode } from 'hono/utils/http-status'
 
 const app = new Hono()
 
@@ -8,20 +10,30 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-
 app.get('bus-timings', async (c) => {
   const url: string = "https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival";
   const busStopCode: number = 83139;
 
-  const response = await fetch(url + `?BusStopCode=${busStopCode}`,
-    { headers: {"AccountKey": process.env.LTA_DATAMALL_API_KEY!}}
-  );
+  try {
+    const response = await fetch(url + `?BusStopCode=${busStopCode}`,
+      { headers : { "AccountKey": process.env.DATAMALL_API_KEY! }}
+    );
+    const status = response.status as ContentfulStatusCode;
 
-  if (!response.ok) {
-    console.log("Error in fetching bus timings");
+    if (!response.ok) {
+      // returns error response and doesnt throw error
+      console.error(response.status, "Error fetching bus timing");
+      throw new HTTPException(status, { message: "Error fetching bus timings" });
+    }
+
+    const data = await response.json();
+    c.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+    return c.json(data);
+
+  } catch (error) {
+      console.error("Error fetching bus timings: ", error);
+      throw new HTTPException(502, { message: "LTA Datamall bus timing error"});
   }
-
-  return c.json(response.json());
 })
 
 serve({
