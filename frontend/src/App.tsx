b37e6116aa5ce36fd9daa71dfc52d33595ab1adb
waylonggggg <tanwaylon38@gmail.com>
2026-05-13 23:56:49 +0800

    Add placeholder navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import BusComponent from "./BusComponent"
+import NavBar from "@/components/NavBar"
 
 function App() {
   return (
     <>
       <section>
-        <h1>bus app</h1>
+        <NavBar/>
         <BusComponent/>
       </section>
     </>
