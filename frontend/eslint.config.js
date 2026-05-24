3007ae702b94810f3e9f74b0f125af1d55fa0a2c
waylonggggg <tanwaylon38@gmail.com>
2026-05-24 23:35:49 +0800

    Add prettier to project

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,19 +4,21 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
+  reactHooks.configs.flat.recommended,
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier
     ],
     languageOptions: {
-      globals: globals.browser,
-    },
-  },
+      globals: globals.browser
+    }
+  }
 ])
