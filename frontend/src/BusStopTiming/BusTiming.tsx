a82f7c4745d4955d65ffc2a7bb99cb3c40c36f4f
waylonggggg <tanwaylon38@gmail.com>
2026-05-25 21:53:58 +0800

    Create utils file for clean data function

--- frontend/src/BusStopTiming/BusTiming.tsx
+++ frontend/src/BusStopTiming/BusTiming.tsx
@@ -2,54 +2,12 @@ import { useEffect, useState } from "react";
 import BusStopList from "@/BusStopTiming/BusStopList";
 import type {
   BusTimingResponse,
-  BusServiceInfo,
   BusStopTimings,
-  NextBusDetails,
-  BusArrivalInfo,
   NearestBusStopResponse,
   Coordinates
 } from "@/types/bus";
+import { cleanData } from "@/utils";
 import { fetchNearestBusStop, fetchBusTiming } from "../api/bus";
-
-function cleanData(data: BusTimingResponse): BusStopTimings {
-  const cleaned: BusServiceInfo[] = data.Services.map((busService) => {
-    const nowTime = new Date().getTime();
-
-    const parseTiming = (estimatedArrival: string) => {
-      const arrivalTime = new Date(estimatedArrival).getTime();
-
-      const timing = Math.floor((arrivalTime - nowTime) / 1000 / 60);
-      return timing <= 0 ? "Arriving" : timing;
-    };
-
-    const parseBusInfo = (nextBusDetails: NextBusDetails): BusArrivalInfo => {
-      const timing = nextBusDetails.EstimatedArrival
-        ? parseTiming(nextBusDetails.EstimatedArrival)
-        : "-";
-      const load = nextBusDetails.Load ? nextBusDetails.Load : "-";
-
-      return {
-        timing: timing,
-        load: load,
-      };
-    };
-
-    return {
-      serviceNo: busService.ServiceNo,
-      nextBusOne: parseBusInfo(busService.NextBus),
-      nextBusTwo: parseBusInfo(busService.NextBus2),
-      nextBusThree: parseBusInfo(busService.NextBus3),
-    };
-  });
-
-  return {
-    busStopCode: data.BusStopCode,
-    busServices: cleaned.sort((a, b) => a.serviceNo.localeCompare(b.serviceNo, "en", {
-      numeric: true,
-      sensitivity: "base"
-    }))
-  };
-}
 
 export default function BusTiming() {
   const [busTiming, setBusTiming] = useState<BusStopTimings>();
---
+++ frontend/src/utils.ts
@@ -0,0 +1,41 @@
+import type { BusServiceInfo, BusStopTimings, BusTimingResponse, NextBusDetails, BusArrivalInfo } from "./types/bus";
+
+export function cleanData(data: BusTimingResponse): BusStopTimings {
+  const cleaned: BusServiceInfo[] = data.Services.map((busService) => {
+    const nowTime = new Date().getTime();
+
+    const parseTiming = (estimatedArrival: string) => {
+      const arrivalTime = new Date(estimatedArrival).getTime();
+
+      const timing = Math.floor((arrivalTime - nowTime) / 1000 / 60);
+      return timing <= 0 ? "Arriving" : timing;
+    };
+
+    const parseBusInfo = (nextBusDetails: NextBusDetails): BusArrivalInfo => {
+      const timing = nextBusDetails.EstimatedArrival
+        ? parseTiming(nextBusDetails.EstimatedArrival)
+        : "-";
+      const load = nextBusDetails.Load ? nextBusDetails.Load : "-";
+
+      return {
+        timing: timing,
+        load: load,
+      };
+    };
+
+    return {
+      serviceNo: busService.ServiceNo,
+      nextBusOne: parseBusInfo(busService.NextBus),
+      nextBusTwo: parseBusInfo(busService.NextBus2),
+      nextBusThree: parseBusInfo(busService.NextBus3),
+    };
+  });
+
+  return {
+    busStopCode: data.BusStopCode,
+    busServices: cleaned.sort((a, b) => a.serviceNo.localeCompare(b.serviceNo, "en", {
+      numeric: true,
+      sensitivity: "base"
+    }))
+  };
+}
